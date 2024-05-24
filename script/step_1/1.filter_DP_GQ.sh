57022f0bbbb95417d1131130e5f92a5f22aa1501
Sujaur-Rahman <104597373+Sujaur-Rahman@users.noreply.github.com>
2024-05-24 03:23:28 -0400

    Update 1.filter_DP_GQ.sh

--- script/step_1/1.filter_DP_GQ.sh
+++ script/step_1/1.filter_DP_GQ.sh
@@ -3,9 +3,9 @@ do
   awk -F'\t' '
     /^#/ {print; next}  # Print all header lines
     {
-      split($10, arr, ":");  # Assuming the first sample is in column 10
-      dp = arr[3];  # Adjust index if DP is not the 3rd subfield
-      gq = arr[4];  # Adjust index if GQ is not the 4th subfield
+      split($10, arr, ":"); 
+      dp = arr[3]; 
+      gq = arr[4]; 
       if (dp >= 20 && gq >= 30) print  # Print lines where DP >= 20 and GQ >= 30
     }
   ' $vcf > "${vcf%.vcf}.filtered.vcf"
